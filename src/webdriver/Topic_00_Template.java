5328574a667cc1ecb5bdb3e0e518a4f968b5348b
nguyenha <nguyentthuha1999@gmail.com>
2022-09-16 21:16:19 +0700

    Create template class

--- src/webdriver/Topic_00_Template.java
+++ src/webdriver/Topic_00_Template.java
@@ -10,7 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_01_Check_Environment {
+public class Topic_00_Template {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
@@ -26,22 +26,28 @@ public class Topic_01_Check_Environment {
 		}
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
 	}
 
 	@Test
-	public void TC_01_() {
-		
+	public void TC_01_ValidateCurrentUrl() {
+		// Login Page Url matching
+		String loginPageUrl = driver.getCurrentUrl();
+		Assert.assertEquals(loginPageUrl, "https://www.facebook.com/");
 	}
 
 	@Test
-	public void TC_02_() {
-		
+	public void TC_02_ValidatePageTitle() {
+		// Login Page title
+		String loginPageTitle = driver.getTitle();
+		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
 	}
 
 	@Test
-	public void TC_03_() {
-
+	public void TC_03_LoginFormDisplayed() {
+		// Login form displayed
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
 	}
 
 	@AfterClass
