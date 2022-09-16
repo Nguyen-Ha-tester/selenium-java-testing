834f540f63a1c04eaacad2e8f7225cac4ae32687
nguyenha <nguyentthuha1999@gmail.com>
2022-09-16 21:44:04 +0700

    TC_01 in Locator

--- src/Javatester/Topic_02_Data_Type.java
+++ src/Javatester/Topic_02_Data_Type.java
@@ -1,15 +1,63 @@
 package Javatester;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Topic_02_Data_Type {
 
 	public static void main(String[] args) {
 		//1. Kiểu dữ liệu nguyên thuỷ (Primitive): Liên quan đến số: 
-	     	// * Số nguyên: byte, short, int, long (k có phần thập phân)
-	    	// * Số thực: float double (có phần thập phân)
-		    // * Logic: boolean (
-	    	// * Kí tự: char (trong character)
+	    
+		    // * Số nguyên (Integer): byte, short, int, long (k có phần thập phân)
+		       // => Mục đích:biểu diễn độ rộng để lưu trữ dữ liệu từ nhỏ đến lớn
+		       byte bNUmber = 127; // giới hạn là -127 -> 127
+		       short sNUmber = 129;
+		       int iNumber = 13123;   // cái này dùng nhiều này
+		       long lNumber = 1322112312; // cái này dùng nhiều này
+		       
+	    	// * Số thực (Floating point): float, double (có phần thập phân)
+		       double Haheight = 1.66d; // giới hạn là từ -3.4E +38 đến 3.4E + 38
+		       float Haheight1 = 1.6f;
+		       
+		    // * Logical: boolean (true or false)
+		       boolean Haisbeautiful = true; // Hà rất xinh  //cái này dùng nhiều này
+		    		   Haisbeautiful = false; // Hà không xinh
+		    		   
+	    	// * Kí tự (symbols): char (trong character)
+		       char a = 'A'; // Ít dùng
 		
-		//2.Kiểu dữ liệu tham chiếu (Reference)
+		//2. Kiểu dữ liệu tham chiếu (Reference)
+		    // Class
+		       FirefoxDriver driver = new FirefoxDriver();
+		       
+		    // Interface
+		       WebElement Aixinhnhat;
+		       
+		    // String
+		       String Aixinhnhat1 = "Haxinh"; // Là cái chứa nhiều giá trị tham chiếu char
+		       
+		    // Object
+		       Object Haxinhh;
+		       
+		    // Array
+		       String []studentname = {"Ha", "Huy"};  //cái này dùng nhiều này => xác định trong xpath xem đâu là chuỗi string, string nằm trong dấu ""
+		       
+		    // Collection: Set, Queue, List
+		       List <WebElement> checkboxes = driver.findElements (By.cssSelector(Aixinhnhat1));
+		       
+		    // Map
+		       Map<String, Integer> student = new HashMap<String, Integer>();
+		       
+		   
+// Sự khác biệt giữa kiểu tham chiếu và nguyên thuỷ: nguyên thuỷ k thay đổi giá trị, còn tham chiếu thì thay đổi
+		      
+		       
 	}
+
 
 }
--- src/webdriver/Topic_02_Selenium_Locator.java
+++ src/webdriver/Topic_02_Selenium_Locator.java
@@ -19,33 +19,59 @@ public class Topic_02_Selenium_Locator {
 	public void beforeClass() {
 		if (osName.contains("Mac OS")) {
 			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
-			
+
 		} else {
 			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 
 		}
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	// Mở trang sign in của rakuna ra
-		driver.get("https://secure.rakuna.co/")
+
+		// Mở trang sign in của rakuna ra
+		driver.get("https://secure.rakuna.co/");
 	}
-//<form class="new_recruiter" id="signin-form" recruiter="{&quot;email&quot;:&quot;&quot;}" action="/recruiter/sign_in" accept-charset="UTF-8" method="post">
-	// Code HTML của Sign in text gồm:
-	//	• Tên thẻ của element (tagname): h1
-	//	• Tên của thuộc tính (Atribute name): class="MuiTypography-root MuiTypography-h4 css-130mlvd"
-	//	• Giá trị của thuộc tính (value): Sign In
+
+	// <form class="new_recruiter" id="signin-form"
+	// recruiter="{&quot;email&quot;:&quot;&quot;}" action="/recruiter/sign_in"
+	// accept-charset="UTF-8" method="post">
+
+	//===> Code HTML bên trên là của trang Sign in gồm:
+	// • Tên thẻ của element (Tagname): form
+	// • Tên của thuộc tính (Atribute name): class="MuiTypography-root
+	// MuiTypography-h4 css-130mlvd"
+	// • Giá trị của thuộc tính (value): Sign In
 	@Test
 	public void TC_01_ID() {
-		driver.findElement(By.id("")):
+		driver.findElement(By.id("signin-form")).sendKeys("haxinhgai");
 	}
 
 	@Test
-	public void TC_02_() {
-		
+	public void TC_02_Class() {
+
 	}
 
 	@Test
-	public void TC_03_() {
+	public void TC_03_Name() {
+
+	}
+	@Test
+	public void TC_04_LinkText() {
+
+	}
+	@Test
+	public void TC_05_PartialLinkText() {
+
+	}
+	@Test
+	public void TC_06_Tagname() {
+
+	}
+	@Test
+	public void TC_07_Css() {
+
+	}
+	@Test
+	public void TC_08_Xpath() {
 
 	}
 
