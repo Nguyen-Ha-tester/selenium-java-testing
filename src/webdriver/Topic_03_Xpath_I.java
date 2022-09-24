63e709640525a4438cd902ee5e7ced8017d2de11
nguyenha <nguyentthuha1999@gmail.com>
2022-09-24 12:58:06 +0700

    Xpath_I

---
+++ src/webdriver/Topic_03_Xpath_I.java
@@ -0,0 +1,53 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_03_Xpath_I {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac OS")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+
+		}
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+	}
+
+	@Test
+	public void TC_01_() {
+		driver.get("http://live.techpanda.org/index.php/customer/account/login/");
+		driver.findElement(By.xpath("/html/body//input[@placeholder='Search entire store here...']")).sendKeys("NguyenHa@");
+		driver.findElement(By.xpath("//div[@class='account-login']//a[@class='button']")).click(); 
+		
+	}
+	@Test
+	public void TC_02_() {
+
+	}
+
+	@Test
+	public void TC_03_() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
