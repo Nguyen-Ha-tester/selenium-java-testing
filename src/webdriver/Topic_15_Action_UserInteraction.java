f9f3d6dbebe750528637925e9f2f489984954c5b
nguyenha <nguyentthuha1999@gmail.com>
2022-11-02 12:31:34 +0700

    Action_Hover

--- src/webdriver/Topic_15_Action_UserInteraction.java
+++ src/webdriver/Topic_15_Action_UserInteraction.java
@@ -5,13 +5,15 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_00_Template {
+public class Topic_15_Action_UserInteraction {
 	WebDriver driver;
+	Actions action;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
 
@@ -25,14 +27,23 @@ public class Topic_00_Template {
 
 		}
 		driver = new FirefoxDriver();
+		action = new Actions(driver);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
+		}
 
 	@Test
-	public void TC_01_() {
+	public void TC_01_Hover() {
+		driver.get("https://automationfc.github.io/jquery-tooltip/");
 		
-	}
-
+		//Hover vào element
+		action.moveToElement(driver.findElement(By.cssSelector("input#age"))).perform(); // cuối phải có perform thì mới chạy được
+		
+		//Bắt tooltip trên window: ctrl \
+		
+		//Verify tooltip xuất hiện
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.ui-tooltip-content")).getText(),"We ask for your age only for statistical purposes.");
+		}
+		//** Chú ý: Khi đang run test, k đc di chuyển chuột hoặc dùng bàn phím
 	@Test
 	public void TC_02_() {
 	}
