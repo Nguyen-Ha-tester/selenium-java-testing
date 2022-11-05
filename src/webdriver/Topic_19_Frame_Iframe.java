396093fc26712842a13dc0226c8fee8eef42ced4
nguyenha <nguyentthuha1999@gmail.com>
2022-11-05 17:11:05 +0700

    Add Javadoc_Iframe

---
+++ src/webdriver/Topic_19_Frame_Iframe.java
@@ -0,0 +1,54 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_19_Frame_Iframe {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac OS")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+
+		}
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void TC_01_Kyna_Iframe () {
+		
+		driver.get("https://skills.kynaenglish.vn/");
+		//Verify cái facebook iframe hiển thị - vẫn là 1 element của trang kyna
+		Assert.assertTrue(driver.findElement(By.xpath("//iframe[@allowtransparency='false']")).isDisplayed());
+		Assert.assertEquals(driver.findElement(By.cssSelector("div._1drq")).getText(),"165K likes");
+		
+	}
+
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+
+		}
+	}
+}
