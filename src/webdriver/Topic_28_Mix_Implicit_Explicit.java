92d36d995f946164d24ddd639930337108dae55d
nguyentthuha <nguyentthuha1999@gmail.com>
2022-12-02 17:04:59 +0700

    Update mix implicit explicit wait

--- src/webdriver/Topic_28_Mix_Implicit_Explicit.java
+++ src/webdriver/Topic_28_Mix_Implicit_Explicit.java
@@ -1,12 +1,10 @@
 package webdriver;
 
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -33,11 +31,12 @@ public class Topic_28_Mix_Implicit_Explicit {
 
 	public void TC_01_Element_Found() {
 		
-		//Trường hợp 1: Element có xuất hiện mà không cần chờ hết TimOut/
+		//Trường hợp 1: Element có xuất hiện mà không cần chờ hết TimeOut/
 		//Dù có set cả 2 loại wait thì cũng không ảnh hưởng gì cả
 		
-		explicitWait = new WebDriverWait(driver, 15);
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		explicitWait = new WebDriverWait(driver, 5);
+	
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
 		// Implicit Wait chỉ apply cho việc findElement thôi
 		// Explicit Wait apply cho các điều kiện của element
@@ -56,12 +55,39 @@ public class Topic_28_Mix_Implicit_Explicit {
 		
 	}
 	
-	@Test
 	public void TC_02_Element_Not_Found_Implicit() {
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
 		driver.get("https://www.facebook.com/");
-		// Implicit
+		
+		// Implicit:
+			//Cách 1:
+			//output khi k tìm thấy
+				// Có cơ chế tìm lại sau mỗi 500 milisecond (0.5s)
+				// Hết timeout sẽ đánh fail testcase này
+				// Và throw ra 1 exception: NoSuchElement
+				// Mà nếu bị fail thì k đến bước tiếp theo được
+				// => Nên phải dùng hàm try, catch. Khi try fail thì sẽ throw ra exception. Sau đó catch sẽ bắt cái exception lại và tiếp tục
+				
+				System.out.println("Thoi gian bat dau cua implicit = " + getTimeStamp());
+				try {
+					driver.findElement(By.id("haxinhgai"));
+				} catch (Exception e) {
+					System.out.println("Thoi gian ket thuc cua implicit = " + getTimeStamp());
+				}
+				}
+			// Cách 2:
+				//List<WebElement> notFoundElement = driver.findElements(By.id("haxinhgai"));
+				//System.out.println("Thoi gian ket thuc cua implicit = " + getTimeStamp());
+		
+	@Test
+	public void TC_03_Element_Not_Found_Implicit_Explicit() {
+//case 1: implicit = explicit
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		explicitWait = new WebDriverWait(driver, 5);
+		driver.get("https://www.facebook.com/");
+		
+		//Implicit
 		System.out.println("Thoi gian bat dau cua implicit = " + getTimeStamp());
 		try {
 			driver.findElement(By.id("haxinhgai"));
@@ -70,18 +96,59 @@ public class Topic_28_Mix_Implicit_Explicit {
 
 		}
 		
-		List<WebElement> notFoundElement = driver.findElements(By.id("haxinhgai"));
-		System.out.println("Thoi gian ket thuc cua implicit = " + getTimeStamp());
-		//output khi k tìm thấy
-		// Có cơ chế tìm lại sau mỗi 500 milisecond (0.5s)
-		// Hết timeout sẽ đánh fail testcase này
-		// Và throw ra 1 exception: NoSuchElement
-		// Mà nếu bị fail thì k đến bước tiếp theo được
-		// => Nên phải dùng hàm try, catch. Khi try fail thì sẽ throw ra exception. Sau đó catch sẽ bắt cái exception lại
-		// và tiếp tục
-		
-		
+		//Explicit:
+		System.out.println("Thoi gian bat dau cua explicit = " + getTimeStamp());
+		try {
+			explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("haxinhgai")));
+		} catch (Exception e) {
+			System.out.println("Thoi gian ket thuc cua explicit = " + getTimeStamp());
 	}
+//case 1: implicit < explicit
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		explicitWait = new WebDriverWait(driver, 10);
+		driver.get("https://www.facebook.com/");
+		
+		//Implicit
+		System.out.println("Thoi gian bat dau cua implicit = " + getTimeStamp());
+		try {
+			driver.findElement(By.id("haxinhgai"));
+		} catch (Exception e) {
+			System.out.println("Thoi gian ket thuc cua implicit = " + getTimeStamp());
+
+		}
+
+		// Explicit:
+		System.out.println("Thoi gian bat dau cua explicit = " + getTimeStamp());
+		try {
+			explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("haxinhgai")));
+		} catch (Exception e) {
+			System.out.println("Thoi gian ket thuc cua explicit = " + getTimeStamp());
+		}
+// case 1: implicit > explicit
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		explicitWait = new WebDriverWait(driver, 5);
+		driver.get("https://www.facebook.com/");
+		
+		//Implicit
+		System.out.println("Thoi gian bat dau cua implicit = " + getTimeStamp());
+		try {
+			driver.findElement(By.id("haxinhgai"));
+		} catch (Exception e) {
+			System.out.println("Thoi gian ket thuc cua implicit = " + getTimeStamp());
+
+		}
+
+		// Explicit:
+		System.out.println("Thoi gian bat dau cua explicit = " + getTimeStamp());
+		try {
+			explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("haxinhgai")));
+		} catch (Exception e) {
+			System.out.println("Thoi gian ket thuc cua explicit = " + getTimeStamp());
+		}
+	}
+	
+				
+	
 	
 	@AfterClass
 	public void afterClass() {
