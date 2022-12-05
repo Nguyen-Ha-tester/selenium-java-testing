a98a65a8369e9b23bcfdea1f533e8893ec6811fd
nguyentthuha <nguyentthuha1999@gmail.com>
2022-12-05 17:22:56 +0700

    Start Fluent Wait

---
+++ src/webdriver/Topic_29_FluentWait.java
@@ -0,0 +1,36 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_29_FluentWait {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac OS")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
+		driver = new FirefoxDriver();
+	}
+
+	@Test
+	public void TC_01_() {
+		
+	}
+	
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
+//Test
