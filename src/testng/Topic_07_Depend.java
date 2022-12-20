9254c26e54a8536c6616d253f7656a746fb8d25a
Ha Nguyen <106964292+hanguyen10061999@users.noreply.github.com>
2022-12-20 17:21:49 +0700

    Test_IV

---
+++ src/testng/Topic_07_Depend.java
@@ -0,0 +1,23 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class Topic_07_Depend {
+
+	@Test(groups = "admin")
+	public void product_01_create_product() {
+	}
+
+	@Test(groups = "admin")
+	public void product_02_view_product() {
+	}
+
+	@Test(groups = "admin")
+	public void product_03_edit_product() {
+	}
+
+	@Test(groups = "admin")
+	public void product_04_delete_product() {
+	}
+
+}
